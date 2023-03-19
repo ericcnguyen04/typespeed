510414f7fc379538d672de6a34b74cfa88b2e8dc
ericcnguyen04 <ericc.win@gmail.com>
2023-03-19 03:26:12 -0400

    startoff

--- src/Components/pages/Type.tsx
+++ src/Components/pages/Type.tsx
@@ -4,7 +4,7 @@ export default function Type() {
     return(
         <div className="display">
             <div className="type">
-            heyooois
+            heyoooisd
             </div>
         </div>
     )
