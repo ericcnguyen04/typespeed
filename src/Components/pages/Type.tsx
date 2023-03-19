0778218eb24b3c706e8692de3297dc5654f6449b
ericcnguyen04 <ericc.win@gmail.com>
2023-03-19 12:36:58 -0400

    break

--- src/Components/pages/Type.tsx
+++ src/Components/pages/Type.tsx
@@ -4,9 +4,27 @@ import "../../App.css"
 import axios from 'axios'
 
 import {FC, useState, useEffect} from "react";
+import { useNavigate, NavigateFunction, useParams } from "react-router-dom"
+
+
 
 const Type=()=> {
     const [words, setWords] = useState<string>('')
+    const [index,setIndex]=useState<number>(0)
+    const [mistakes,setMistakes]=useState<string[]>([])
+    const [time, setTime] = useState<number>(60)
+    const [wpm,setWpm]=useState<number>(0)
+    const [suggestionUrl,setSuggestionUrl]=useState<string>('')
+
+
+    let navigate:NavigateFunction = useNavigate()
+
+    const handleNewTest =()=>{
+        // resetting the url based on suggested from test results then refreshes
+        navigate(suggestionUrl) 
+        navigate(0)
+    }
+    
         return(
             <div className="display">
                 <div className="type">
