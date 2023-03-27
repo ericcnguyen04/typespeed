d13b15c42c37b5509b3df71cd96d0618f534aa2c
ericcnguyen04 <ericc.win@gmail.com>
2023-03-27 11:55:25 -0400

    heres the start

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,22 @@ import Aside from './Components/partials/Aside'
 import Nav from './Components/partials/Nav'
 import Type from './Components/pages/Type'
 
+import { Router, Route, Routes } from "react-router-dom"
+
 function App() {
   return (
-    <div className="App">
-      <Aside />
-      <Nav />
+    <Router>
+      <Routes>
+        <Route></Route>
 
-      <Type />
-    </div>
+        <div className="App">
+          <Aside />
+          <Nav />
+
+          <Type />
+        </div>
+      </Routes>
+    </Router>
   );
 }
 
